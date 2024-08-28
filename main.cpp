6580d5bb66625c525caf4c82e4f9f33936152efc
crazykyler005 <k.s.nelson77@gmail.com>
2024-08-28 14:18:44 -0800

    fixed import

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 // #include "chip8.hpp"
 #include "window.hpp"
-//chip8 myChip8;
 #include <cstdio>
 #include <stdint.h>
 
-// g++ `pkg-config --cflags gtkmm4` -o start main.cpp window.cpp `pkg-config --libs gtk4`
-// g++ `pkg-config --cflags gtkmm-4.0` -o test main.cpp test2.cpp screen.cpp menubar.cpp `pkg-config --libs gtkmm-4.0`
+// g++ `pkg-config --cflags gtkmm-4.0` -o test main.cpp window.cpp screen.cpp menubar.cpp `pkg-config --libs gtkmm-4.0`
 
 int main(int argc, char *argv[])
 {
